6f5c75619f2b847fb1e388949d1ca25d16885d78
abdo ragab hashem <ar1813@fayoum.edu.eg>
2020-11-07 23:04:39 +0200

    detect node without edges

---
+++ graph.py
@@ -0,0 +1,100 @@
+from maze_map import Map_point
+
+class Node(object):
+    def __init__(self, id: int, map_point: Map_point = (-1, -1)):
+        self.id = id
+        self.edges = []
+        self.map_point = map_point
+
+
+class Edge(object):
+    def __init__(self, id:int, node_from:Node, node_to:Node, distance=-1):
+        self.id = id
+        self.node_from = node_from
+        self.node_to = node_to
+        self.distance = distance
+
+
+class Graph(object):
+    def __init__(self, nodes=[], edges=[]):
+        self.nodes = nodes
+        self.edges = edges
+
+    def insert_node(self, new_node_id, location: Map_point):
+        new_node = Node(new_node_id, location)
+        self.nodes.append(new_node)
+    
+
+    def insert_edge(self, new_edge_id, node_from_id, node_to_id):
+        from_found = None
+        to_found = None
+        for node in self.nodes:
+            if node_from_id == node.id:
+                from_found = node
+            if node_to_id == node.id:
+                to_found = node
+        if from_found == None:
+            from_found = Node(node_from_id)
+            self.nodes.append(from_found)
+        if to_found == None:
+            to_found = Node(node_to_id)
+            self.nodes.append(to_found)
+        new_edge = Edge(new_edge_id, from_found, to_found)
+        from_found.edges.append(new_edge)
+        to_found.edges.append(new_edge)
+        self.edges.append(new_edge)
+
+    def get_edge_list(self):
+        """Don't return a list of edge objects!
+        Return a list of triples that looks like this:
+        (Edge id, From Node id, To Node id)"""
+        return [(e.id, e.node_from.id, e.node_to.id) for e in self.edges]
+        
+
+    def get_adjacency_list(self):
+        """Don't return any Node or Edge objects!
+        You'll return a list of lists.
+        The indecies of the outer list represent
+        "from" nodes.
+        Each section in the list will store a list
+        of tuples that looks like this:
+        (To Node, Edge id)"""
+        
+        adjc_list = [None]*(len(self.nodes)+1)
+        for node in self.nodes:
+            row = []
+            for e in node.edges:
+                if e.node_to != node:
+                    row.append((e.node_to.id, e.id))
+            if row:
+                adjc_list[node.id] = row
+            
+        return adjc_list
+
+    def get_adjacency_matrix(self):
+        """Return a matrix, or 2D list.
+        Row numbers represent from nodes,
+        column numbers represent to nodes.
+        Store the edge ids in each spot,
+        and a 0 if no edge exists."""
+        n_nodes = len(self.nodes)+1
+        adj_mat = [[0]*n_nodes for i in range(n_nodes)]
+        for e in self.edges:
+            adj_mat[e.node_from.id][e.node_to.id] = e.id
+        
+        return adj_mat
+
+
+if __name__ == "__main__":
+    # test
+    graph = Graph()
+    graph.insert_edge(100, 1, 2)
+    graph.insert_edge(101, 1, 3)
+    graph.insert_edge(102, 1, 4)
+    graph.insert_edge(103, 3, 4)
+    # Should be [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
+    print (graph.get_edge_list())
+    # Should be [None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None]
+    print (graph.get_adjacency_list())
+    # Should be [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
+    print (graph.get_adjacency_matrix())
---
+++ maze_map.py
@@ -0,0 +1,177 @@
+from dataclasses import dataclass
+from enum import Enum
+import copy
+from graph import Graph
+
+class Map_el(Enum):
+    WALL = '%'
+    PLAYER = 'P'
+    TRAGET = '.'
+    EMPTY = ' '
+
+
+@dataclass
+class Location:
+    x: int
+    y: int
+
+
+@dataclass
+class Map_point:
+    content: Map_el
+    location: Location
+
+
+class Maze_map:
+
+    def __init__(self, path):
+        self.layout = self.load_map(path)
+        self.hight = len(self.layout)
+        self.width = len(self.layout[0])
+        self.player = self.find(Map_el.PLAYER)
+        self.traget = self.find(Map_el.TRAGET)
+        self.graph = self.build_graph()
+
+    def load_map(self, path) -> Map_point:
+        with open("Maze/tinySearch.txt") as maze_file:
+            maze_map = maze_file.read()
+            maze_map = maze_map.split("\n")
+            maze_map = [list(row) for row in maze_map]
+
+
+
+        self.layout = self._sympol_to_map_point(maze_map)
+        return self.layout
+
+    def get_point_by_location(self, location: Location) -> Map_point:
+        return self.layout[location.y][location.x]
+    
+    def build_graph(self):
+        graph = Graph()
+        counter_id = 0
+        for row in self.layout:
+            for point in row:
+                graph.insert_node(counter_id, point)
+
+        # TODO find edges
+        # 1. start from player postion 
+        # 2. use x-axis and y-axis to find @next_node
+        # 3. measure distance 
+        # 4. add edge 
+        # 5. contenue with next from @next_nodes at [2]
+        return graph
+
+    
+    
+    def _sympol_to_map_point(self,maze_map):
+        maze_map = copy.deepcopy(maze_map)
+        for row_index, row in enumerate(maze_map):
+            for col_index, symbol in enumerate(row):
+                map_point = Map_point(Map_el(symbol),
+                                      Location(col_index, row_index))
+                maze_map[row_index][col_index] = map_point
+        
+        return maze_map
+    
+    def find(self, content : Map_el):
+        res = []
+        for row in self.layout:
+            for point in row:
+                if point.content == content:
+                    res.append(point)
+        return res
+
+    def _map_point_to_sympol(self, maze_map):
+        ''' convert a Map_point map to Sympol
+        Args: 
+            maze_map List<Map_point> 
+        '''
+        maze_map = copy.deepcopy(maze_map)
+        for row_index, row in enumerate(maze_map):
+            for col_index, point in enumerate(row):
+                maze_map[row_index][col_index] = point.content.value
+
+        return maze_map
+
+    def _marked_nodes(self):
+        maze = self._map_point_to_sympol(self.layout)
+        
+        for row in self.layout:
+            for point in row:
+                if self.is_intersection(point.location):
+                    maze[point.location.y][point.location.x] = '*'
+                if self.is_end_point(point.location):
+                    maze[point.location.y][point.location.x] = '^'
+
+        return maze
+
+    def __str__(self):
+
+        maze = 'original map \n'
+        for row in self._map_point_to_sympol(self.layout):
+            maze += ''.join(row)+'\n'
+
+        # important points 
+        maze += 'marked map \n'
+        for row in self._marked_nodes():
+            maze += ''.join(row)+'\n'
+
+        return maze
+
+
+    def point_in_top(self, location: Location) -> Map_point:
+        if location.y == 0:
+            return None
+        else:
+            return self.get_point_by_location(Location(location.x, location.y-1))
+
+    def point_in_bottom(self, location: Location) -> Map_point:
+        if location.y == self.hight-1:
+            return None
+        else:
+            return self.get_point_by_location(Location(location.x, location.y+1))
+
+    def point_in_right(self, location: Location) -> Map_point:
+        if location.x == self.width-1:
+            return None
+        else:
+            return self.get_point_by_location(Location(location.x+1, location.y))
+
+    def point_in_left(self, location: Location) -> Map_point:
+        if location.x == 0:
+            return None
+        else:
+            return self.get_point_by_location(Location(location.x-1, location.y))
+
+    def nighbors_mat(self, location: Location):
+        ''' array  of nighbors in order [top , right, bottom, left] '''
+        return [self.point_in_top(location),
+                self.point_in_right(location),
+                self.point_in_bottom(location),
+                self.point_in_left(location)]
+
+    def is_intersection(self, location: Location) -> bool:
+        if self.get_point_by_location(location).content == Map_el.WALL:
+            return False
+        
+        available_pathes = 0
+        for point in self.nighbors_mat(location):
+            if point is not None and point.content !=Map_el.WALL:
+                available_pathes += 1
+        
+        return available_pathes >= 3
+
+
+    def is_end_point(self, location: Location) -> bool:
+        if self.get_point_by_location(location).content == Map_el.WALL:
+            return False
+            
+        blocked_pathes = 0
+        for point in self.nighbors_mat(location):
+            if point is None or point.content == Map_el.WALL:
+                blocked_pathes +=1
+        
+        return blocked_pathes >= 3
+
+    def is_trget(self, location) -> bool:
+        return self.get_point_by_location(location).content == Map_el.TRAGET
