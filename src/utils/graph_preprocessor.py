89d04e4e0b427de234a044e08564a2b1e02e16ca
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-11-08 12:41:09 +0200

    fix: add missing starting node

--- src/utils/graph_preprocessor.py
+++ src/utils/graph_preprocessor.py
@@ -4,15 +4,17 @@ def graph_algorithm(preprocessor=None):
         a graph decorator which checks for graph validity and do preprocessing 
         if supplied
         NOTE: the preprocessor is function that return a preprocessed graph
+        NOTE: the graph algorithm is excepted to have a graph as the first argument
+        and a starting point as the second one
     """
     def decorator(function):
         def wrapper(*args, **kwargs):
             graph = args[0]
             if len(graph.nodes) and not len(graph.edges):
-                raise GraphStructureError("The graph have unconnected nodes")
+                raise GraphStructureError("all nodes are not connected")
             if preprocessor :
                 graph = preprocessor(graph) 
-            result = function(graph)
+            result = function(graph,args[1])
             return result
         return wrapper
     return decorator
