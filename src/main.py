2dc92b8b7f7ca3d9bfabda5a0fe1e0b5dc4ce1ff
abdo ragab hashem <ar1813@fayoum.edu.eg>
2020-11-08 22:29:47 +0200

    test: print debug info at main

--- src/main.py
+++ src/main.py
@@ -4,4 +4,10 @@ from models.maze_map import Maze_map
 
 
 maze_map = Maze_map('Maze/tinySearch.txt')
+
 print(maze_map)
+for i in maze_map.graph.get_adjacency_list():
+    print(i)
+for i in maze_map.node_map():
+    print(i)
+
