9487284cd617bc444c5dc95bf788afc9f387e2bd
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-11-09 00:10:27 +0200

    feat: create lazy loaded adj list BFS

--- src/algorithms/BFS.py
+++ src/algorithms/BFS.py
@@ -2,17 +2,17 @@ from models.graph import Graph
 from utils.graph_preprocessor import graph_algorithm
 
 
+# TODO: try to make a generic BFS
 @graph_algorithm()
-def BFS(graph: Graph, starting_node_index, callback=lambda node: print(f"Node Id = {node.id}")):
+def BFS(graph: Graph, starting_node_id, callback=lambda node: print(f"Node Id = {node.id}")):
     """
     Do a BFS over a graph
-
     NOTE: callback is a function that gets called after each node being visited
     NOTE: callback is expected to take a Node object only as an argument
     """
     visited_nodes = set()
     adjacency_list = graph.get_adjacency_list()
-    nodes_queue = [starting_node_index]
+    nodes_queue = [starting_node_id]
     while len(nodes_queue):
         node = nodes_queue.pop(0)  # remove the first element, FIFO
         if node not in visited_nodes:
@@ -23,3 +23,23 @@ def BFS(graph: Graph, starting_node_index, callback=lambda node: print(f"Node Id
                 for connection in connections:
                     # add the to index to nodes queue
                     nodes_queue.append(connection[0])
+
+
+def lazyBFS(starting_node_id, get_adjacent_nodes_ids,
+             callback=None):
+    """
+        Do BFS but with lazy adjacency list loading, suitable for the unusual 
+        adjacency list mechanism like the maze-graph converter or getting the
+        adjacent list from an external source, for example getting them online
+    """
+    visited_nodes = set()
+    nodes_queue = [starting_node_id]
+    while len(nodes_queue):
+        node = nodes_queue.pop(0)
+        if node not in visited_nodes:
+            visited_nodes.add(node)
+            if callback:
+                callback(node)
+            adjacent_nodes = get_adjacent_nodes_ids(node)
+            if adjacent_nodes :
+                nodes_queue.extend(adjacent_nodes)
