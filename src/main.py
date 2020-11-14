7055fc30ff5de0b5d9717d8109453f0a337dc1e3
abdo ragab hashem <ar1813@fayoum.edu.eg>
2020-11-14 10:46:09 +0200

    fix: comment main

--- src/main.py
+++ src/main.py
@@ -12,26 +12,23 @@ mazes = [
 
 maze_map = Maze_map(f'Maze/{mazes[-1]}')
 
-# maze_map = Maze_map('Maze/tinySearch.txt')
+#GFS solver
+# starting_point = maze_map.get_node_by_map_point(maze_map.player).id
+# sol = GFS_Solver(starting_point)
+# GFS(maze_map.graph,starting_point,maze_map, sol.solve)
+# print(sol.get_path())
+
+
+# BFS soluation
+# print("start from 0")
+# starting_point = maze_map._get_node_by_location(maze_map.player.location).id
+# sol = BFS_Solver(starting_point, maze_map.graph, maze_map)
+# BFS(maze_map.graph, starting_point, sol.solver)
+# print('result is: ',sol.get_result())
 # print(maze_map)
 
-# BFS(g,9)
-starting_point = maze_map.get_node_by_map_point(maze_map.player).id
-sol = GFS_Solver(starting_point)
-GFS(maze_map.graph,starting_point,maze_map, sol.solve)
-print(sol.get_path())
-# print("start from 1")
-# BFS(g,1)
-# print("start from 2")
-# BFS(g,2)
-print("start from 0")
-starting_point = maze_map._get_node_by_location(maze_map.player.location).id
-sol = BFS_Solver(starting_point, maze_map.graph, maze_map)
-BFS(maze_map.graph, starting_point, sol.solver)
-print('result is: ',sol.get_result())
-print(maze_map)
 
-
+# test map convertion
 # print(maze_map)
 # for i in maze_map.graph.get_adjacency_list():
 #     print(i)
@@ -39,6 +36,14 @@ print(maze_map)
 #     print(i)
 
 # Solver code
+
+def solve_a_star(maze_map):
+    player_point = maze_map.player
+    start_node_id = maze_map._get_node_by_location(player_point.location).id
+    moving_map = a_star(maze_map, start_node_id)
+    for k in moving_map.keys():
+        print(k, moving_map[k])
+
 
 def solve_dfs(maze_map):
     player_point = maze_map.player
@@ -55,3 +60,12 @@ def solve_dfs(maze_map):
 #     maze_map = Maze_map(f'Maze/{map_}')
 #     print(maze_map)
 #     solve_dfs(maze_map)
+
+
+# a_star solution
+# for map_ in mazes:
+# # map_ = mazes[2]
+# # map_ = mazes[-1]
+#     maze_map = Maze_map(f'Maze/{map_}')
+#     print(maze_map)
+#     print(solve_a_star(maze_map))
