b58d50b2528c33227f61fc7521561280ee9bf95d
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-11-14 17:45:40 +0200

    feat: cost cal for gfs

--- src/algorithms/GFS_Solver.py
+++ src/algorithms/GFS_Solver.py
@@ -1,14 +1,18 @@
 from models.graph import Node
+from utils.get_total_cost import get_total_cost
 
 
 class GFS_Solver():
-    def __init__(self, starting_point):
+    def __init__(self, graph ,starting_point):
         self._path = {}
         self._temp_list = []
         self.expansion = [starting_point]
         self.steps = 0
+        self._traversed_nodes = []
+        self.graph = graph
     def solve(self, node: Node):
         id = node.id
+        self._traversed_nodes.append(id)
         if self.expansion[-1] != id:
             self.expansion.append(id)
         if not node.is_target():
@@ -23,3 +27,7 @@ class GFS_Solver():
 
     def steps_counter(self):
         self.steps += 1
+    
+    def total_cost(self):
+        nodes = [self.graph.nodes[node] for node in self._traversed_nodes]
+        return get_total_cost(nodes)
--- src/main.py
+++ src/main.py
@@ -15,20 +15,23 @@ maze_map = Maze_map(f'Maze/{mazes[-1]}')
 
 starting_point = maze_map.get_node_by_map_point(maze_map.player).id
 
-#GFS solver
-# sol = GFS_Solver(starting_point)
-# informed_multi_target_solver(GFS,maze_map.graph,starting_point,maze_map, sol.solve, sol.steps_counter)
-# print('result path',sol.get_path())
-# print('result expansion',sol.expansion)
-# print('#steps: ',sol.steps)
+# GFS solver
+graph = maze_map.graph
+sol = GFS_Solver(graph, starting_point)
+informed_multi_target_solver(
+    GFS, graph, starting_point, maze_map, sol.solve, sol.steps_counter)
+print('result path', sol.get_path())
+print('result expansion', sol.expansion)
+print('#steps: ', sol.steps)
+print('total cost', sol.total_cost())
 
 
 # BFS solution
-sol = BFS_Solver(starting_point, maze_map.graph, maze_map)
-BFS(maze_map.graph, starting_point, sol.solver, sol.steps_counter)
-print('result is: ',sol.get_result())
-print('expansion is: ',sol.expansion)
-print('#steps: ',sol.steps)
+# sol = BFS_Solver(starting_point, maze_map.graph, maze_map)
+# BFS(maze_map.graph, starting_point, sol.solver, sol.steps_counter)
+# print('result is: ',sol.get_result())
+# print('expansion is: ',sol.expansion)
+# print('#steps: ',sol.steps)
 
 # print(maze_map)
 
